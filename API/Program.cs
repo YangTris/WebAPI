e49cbca2e70ca801677d278f55913f04117dba30
Dương Văn Trí <id.dvantri911@gmail.com>
2025-03-26 09:51:56 +0700

    Add: Admin and User role

--- API/Program.cs
+++ API/Program.cs
@@ -63,5 +63,17 @@ app.MapControllers();
 //{
 
 //});
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var roles= new[] { "Admin", "User" };
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(n ew IdentityRole(role));
+        }
+    }
+}
 
 app.Run();
