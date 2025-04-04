aacc2040201ed8828f0b6bc26a9342fffe876cc7
Dương Văn Trí <id.dvantri911@gmail.com>
2025-04-04 09:10:40 +0700

    Update Category.cs

--- Domain/Category.cs
+++ Domain/Category.cs
@@ -7,7 +7,6 @@ namespace Domain
         [Key]
         public int CategoryId { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
         public virtual ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
